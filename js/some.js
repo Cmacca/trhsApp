171b3469218c57707be48e51e91e3ac429f139eb
Krish Dholakiya <krishna.dholakiya@gmail.com>
2013-07-04 17:54:50 -0600

    Initial Upload

---
+++ js/some.js
@@ -0,0 +1,30 @@
+/********************************************************************************
+FUNCTION FOR CROSS DOMAIN AJAX REQUEST USING JQUERY 1.5+
+********************************************************************************/  
+function loadContents(url, callback) {  
+ 
+    //CONFIRM A URL WAS PROVIDED  
+    if(url) {  
+ 
+	    //SET URL FOR YAHOO YQL QUERY
+	    var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + url + '"') + '&format=xml&callback=?';  
+ 
+		//MAKE YAHOO YQL QUERY  
+	   	$.getJSON(yql,function(data) {
+ 
+			//BUILD CALLBACK FUNCTION
+			if(typeof callback === 'function') {  
+			    callback(data.results[0]);  
+			}  
+ 
+		//WRITES ERROR TO LOG	
+	    }).error(function(jqXHR, textStatus, errorThrown) { 
+	    	console.log(errorThrown); }
+	    );
+ 
+  	//LOG ERROR IF NO URL WAS PASSED TO THE SCRIPT
+  	} else {
+  		 console.log('No site was passed to the script.'); 
+  	}
+ 
+} 
