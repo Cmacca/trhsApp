e56f9843a60feddd5538de48844480b643df08dd
Krish Dholakiya <krishna.dholakiya@gmail.com>
2013-07-10 17:14:47 -0600

    Create sched.js

---
+++ www/js/sched.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    
+  //$('#sub').click(function() {
+    
+    $('#sub').click(function() {
+    var clist = $('#form').serializeArray();
+         localStorage.uno = clist[0]['value'];
+         localStorage.dos = clist[1]['value'];
+         localStorage.tres= clist[2]['value'];
+         localStorage.quatro= clist[3]['value'];
+         localStorage.cinco= clist[4]['value'];
+         localStorage.six= clist[5]['value'];
+         localStorage.seven= clist[6]['value'];
+         localStorage.eight= clist[7]['value'];
+         
+    });
